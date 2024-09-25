b0a28ba4787ea3159bbd190f65d857065ed3b0f8
Benedicta Davour <benedictadavour777@gmail.com>
2024-09-25 04:35:33 +0000

    removed second js, started working on gender selection

--- second.js
+++
@@ -1,7 +0,0 @@
-const postUrl = 'https://jsonplaceholder.typicode.com/posts';
-
-const url="www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
-
-const response = fetch(postUrl);
-const cocktail = response.then((data) => console.log(data.json()));
-console.log(cocktail);
